bbf5e4224dfa7058ed79378624a2ab7d7e90fbdc
Ahmet Burak Karhan <aburakkarhan@gmail.com>
2024-12-06 15:37:44 +0300

    feat: implement debounced search functionality in Search component; optimize query parameter handling with improved performance

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,23 +2,24 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams()
   const pathname = usePathname();
-  const { replace } = useRouter()
-  
-  const handleSearch = (item: string) => {
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((item: string) => {
     const params = new URLSearchParams(searchParams)
 
-    if(item) {
+    if (item) {
       params.set('query', item)
     } else {
       params.delete('query')
     }
 
     replace(`${pathname}?${params.toString()}`)
-  }
+  }, 300)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
